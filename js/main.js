778bb0385d1d25b6d3864c60e32dcf22e48e8f44
Franco Aguilo <developer.frag@gmail.com>
2022-09-13 21:38:03 -0300

    Correccion

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
+let finDeCiclo = prompt ('Ingrese ESC para salir');
+finDeCiclo = finDeCiclo.toUpperCase();
 let producto = parseInt(prompt("cantidad de producto deseado"));
 let cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
-let valor = 100;
+//$100 es el valor estatico del producto en el cual baso el simulador
+const valorCuota = (a, b) => 100 * a + 100 * a * (b / 100);
+const datoValido = (a, b) => (a > 0 && b == 1) || b == 3 || b == 6 || b == 12;
+const fin = (a) => a != 'ESC';
 
-while (producto > 0) {
-  switch (cuotas) {
-    case 1:
-      valor = valor * producto;
-      alert("El valor de la cuota es: " + valor);
-      break;
-    case 3:
-      valor = valor * producto + valor * producto * 0.03;
-      alert("El valor de la cuota es: " + valor);
-      break;
-    case 6:
-      valor = valor * producto + valor * producto * 0.06;
-      alert("El valor de la cuota es: " + valor);
-      break;
-    case 12:
-      valor = valor * producto + valor * producto * 0.12;
-      alert("El valor de la cuota es: " + valor);
-      break;
-    default:
-      alert("Cuotas o cantidad de producto invalido");
-      break;
+do {
+  if(fin(finDeCiclo)){while (datoValido(producto, cuotas) && fin(finDeCiclo)) {
+    let valor = 0;
+    switch (cuotas) {
+      case 1:
+        valor = valorCuota(producto, cuotas);
+        alert("El valor de la cuota es: " + valor);
+        break;
+      case 3:
+        valor = valorCuota(producto, cuotas);
+        alert("El valor de la cuota es: " + valor);
+        break;
+      case 6:
+        valor = valorCuota(producto, cuotas);
+        alert("El valor de la cuota es: " + valor);
+        break;
+      case 12:
+        valor = valorCuota(producto, cuotas);
+        alert("El valor de la cuota es: " + valor);
+        break;
+    }
+    producto = parseInt(prompt("cantidad de producto deseado"));
+    cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
+    finDeCiclo = prompt ('Ingrese ESC para salir');
+    finDeCiclo = finDeCiclo.toUpperCase();
   }
+  }else {
+    break;
+  }
+  while (!datoValido(producto, cuotas)) {
+    alert(
+      "los datos ingresados son incorrectos, por favor, vuelva a ingresar los datos"
+    );
+    producto = parseInt(prompt("cantidad de producto deseado"));
+    cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
+  }
+} while (datoValido(producto, cuotas) && fin(finDeCiclo));
 
-  producto = parseInt(prompt("cantidad de producto deseado"));
-  cuotas = parseInt(prompt("cantidad de cuotas deseadas, 1, 3, 6 o 12"));
-  valor = 100;
-}
-
-alert("Cuotas o cantidad de producto invalido recargue la paguina pulsando F5");
